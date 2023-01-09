4155540afe2fa224e1696c2657f18a1e57f7b5ff
choihani <hani_haha@naver.com>
2023-01-09 23:07:49 +0900

    [기능추가] 회원 VO 추가

---
+++ ssg4/src/main/java/com/ssg4/be/member/model/Member.java
@@ -0,0 +1,16 @@
+package com.ssg4.be.member.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private String mId;
+    private String mName;
+    private String mNickname;
+    private String mType;
+    private String mTel;
+    private String mAdr;
+    private String mPostnum;
+}
