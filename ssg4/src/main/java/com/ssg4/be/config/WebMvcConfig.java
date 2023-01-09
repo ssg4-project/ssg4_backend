9ab465c8cac3a216cb27587bc9e75d58e36756ae
choihani <hani_haha@naver.com>
2023-01-09 23:11:12 +0900

    [기능추가] 회원 인증 인터셉터 추가

---
+++ ssg4/src/main/java/com/ssg4/be/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.ssg4.be.config;
+
+import com.ssg4.be.config.interceptor.MainInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MainInterceptor())
+                // 인증 관련
+                .excludePathPatterns("/auth/**")
+                // swagger
+                .excludePathPatterns(
+                        "/swagger-resources/**",
+                        "/swagger-ui/**",
+                        "/v2/api-docs")
+                .addPathPatterns("/*/**");
+    }
+}
